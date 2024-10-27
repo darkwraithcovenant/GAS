387d3e543d3239c6457f45e65667548a5f123a81
Jose De Lara <13313718+darkwraithcovenant@users.noreply.github.com>
2024-10-26 23:09:13 -0700

    .

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -9,7 +9,8 @@ AAuraCharacterBase::AAuraCharacterBase()
 	PrimaryActorTick.bCanEverTick = false;
 
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
-
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
