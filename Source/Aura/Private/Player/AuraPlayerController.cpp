db815ccfc457d360c43620bf41d95d3b934c7c66
Jose De Lara <13313718+darkwraithcovenant@users.noreply.github.com>
2024-10-29 10:55:24 -0700

    .

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,57 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	// set bReplicates boolean to true. makes it so change replicates to the server in multiplayer. makes sure this is
+	//replicated entity 
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	//Assert that halts execution if AuraContext is not set.
+	check(AuraContext);
+	//Set up Subsystem for Input Mode
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem)
+	Subsystem-> AddMappingContext(AuraContext, 0);
+	//Set up our cursor mode settings. 
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	FInputModeGameAndUI InputModeData;
+	//As soon as cursor captured into viewport, we will not hide the cursor
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	// to use input mode data we use player controller function set input mode and pass in the input mode data.
+	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	//Will crash if this cast to InputComponent fails. same as assert. 
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+//AAuraPlayerController - This is a custom PlayerController class
+//Move function handles movement input
+//const FInputActionValue& InputActionValue - This is the parameter that receives input data from Unreal's Enhanced Input System (the new input system)
+//FVector2d InputAxisVector = InputActionValue.Get<FVector2D>() - This line:
+//Gets the input value as a 2D vector (x and y coordinates)
+//Usually, X represents left/right movement
+//Y represents forward/backward movement
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2d InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation (0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X); 
+}
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -5,7 +5,9 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
-
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 /**
  * 
  */
@@ -13,5 +15,36 @@ UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
 	
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+private:
+	//UPROPERTY() - This is a macro that:
+	//Makes the property visible to Unreal's reflection system
+	//Allows the property to show up in the editor
+	//Enables garbage collection for this property
+	//Enables serialization (saving/loading)
+	//EditAnywhere - Allows this property to be edited in:
+	//The details pane, Blueprint editor, Any property window
+	//Category="Input" - Groups this property under an "Input" category in the editor
+	UPROPERTY(EditAnywhere,Category="Input");
+	//declare a pointer to an Input Mapping Context in Unreal Engine's Enhanced Input System.
+	//TObjectPtr<UInputMappingContext>:
+	//UInputMappingContext is a class that defines what inputs (keys, buttons, etc.) trigger which Input Actions
+	//	TObjectPtr is UE5's smart pointer wrapper (safer than raw pointers)
+	//AuraContext - The variable name
+	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere,Category="Input");
+	TObjectPtr<UInputAction> MoveAction;
+	//void return type, returns no value
+	// Move is the function's name
+	// FInputActionValue is an Unreal Engine class that stores input data
+	// const means the parameter cannot be modified inside the function
+	//& means it's passed by reference (to avoid copying the whole object)
+	//InputActionValue - This is just the parameter name
+	void Move(const FInputActionValue& InputActionValue);
 };
