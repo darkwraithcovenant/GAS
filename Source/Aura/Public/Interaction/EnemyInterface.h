15327a50137c8b9131e727870838e5bbdd42012f
darkwraithcovenant <joseop@hotmail.com>
2024-11-01 02:29:38 -0700

    .

--- Source/Aura/Public/Interaction/EnemyInterface.h
+++ Source/Aura/Public/Interaction/EnemyInterface.h
@@ -16,10 +16,28 @@ class UEnemyInterface : public UInterface
 /**
  * 
  */
+
+//class AURA_API IEnemyInterface:
+
+//AURA_API is a macro for DLL export/import (for your game module)
+//I prefix is a naming convention for interfaces in Unreal
+//This interface will be used for enemy-related functionality
 class AURA_API IEnemyInterface
 {
+	//GENERATED_BODY():
+
+	//Unreal macro that generates necessary boilerplate code
+	//Required for Unreal's reflection sys
 	GENERATED_BODY()
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+	virtual void HighlightActor() = 0;
+	virtual void UnHighlightActor() = 0;
+
 };
+
+//Key benefits of using interfaces:
+//Decoupling: Code that uses the interface doesn't need to know specific enemy types
+//Polymorphism: Different classes can implement the interface differently
+//Consistency: Enforces a contract that all enemy classes must follow
