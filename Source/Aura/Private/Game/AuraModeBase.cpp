93525b03bfad93af623d036c14bdf0e0323b33bb
darkwraithcovenant <joseop@hotmail.com>
2024-10-31 17:09:50 -0700

    Added Aura Game Mode Base

---
+++ Source/Aura/Private/Game/AuraModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright DWC Industries
+
+
+#include "Game/AuraModeBase.h"
+
---
+++ Source/Aura/Public/Game/AuraModeBase.h
@@ -0,0 +1,17 @@
+// Copyright DWC Industries
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
